963a5c9c78937e2d1f72f86f77f44678165155b1
Leandro Rocha <leandrocdarocha@gmail.com>
2021-06-14 11:51:06 -0300

    Initial commit

---
+++ src/test/java/org/leandror/jaya/exchange_rate/ExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.leandror.jaya.exchange_rate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
