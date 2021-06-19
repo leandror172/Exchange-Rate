697ec87725da580f454335b475d4a1faeef20022
Leandro Rocha <leandrocdarocha@gmail.com>
2021-06-19 18:16:13 -0300

    Add builder deserialization

--- src/main/java/org/leandror/jaya/exchange_rate/dtos/MonetaryAmount.java
+++ src/main/java/org/leandror/jaya/exchange_rate/dtos/MonetaryAmount.java
@@ -8,14 +8,20 @@ import org.leandror.jaya.exchange_rate.validators.annotations.ValidCurrencyCode;
 import org.springframework.format.annotation.NumberFormat;
 import org.springframework.format.annotation.NumberFormat.Style;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @Schema
 @Builder(setterPrefix = "with")
+@RequiredArgsConstructor
+@JsonDeserialize(builder = MonetaryAmount.MonetaryAmountBuilder.class)
 public class MonetaryAmount {
 
   @NotNull
