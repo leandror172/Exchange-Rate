34cc6cbe96782113e4de153944fbd7988fc189a5
Leandro Rocha <leandrocdarocha@gmail.com>
2021-06-19 12:08:12 -0300

    Test conversion value, refactoring

--- src/test/java/org/leandror/jaya/exchange_rate/services/impl/ConversionServiceImplTest.java
+++ src/test/java/org/leandror/jaya/exchange_rate/services/impl/ConversionServiceImplTest.java
@@ -1,5 +1,6 @@
 package org.leandror.jaya.exchange_rate.services.impl;
 
+import static java.math.BigDecimal.ONE;
 import static java.math.BigDecimal.TEN;
 import static java.time.Instant.ofEpochMilli;
 import static java.time.LocalDateTime.ofInstant;
@@ -12,7 +13,6 @@ import static org.mockito.Mockito.when;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -38,6 +38,13 @@ import io.github.glytching.junit.extension.random.RandomBeansExtension;
 @SpringBootTest(classes = {})
 class ConversionServiceImplTest {
 
+  private static final BigDecimal RATE_BRL_TO_EUR = new BigDecimal(5);
+  private static final BigDecimal RATE_USD_TO_EUR = new BigDecimal(2);
+  private static final String TIME_ZONE_UTC = "UTC";
+  private static final String CURRENCY_CODE_BR = "BRL";
+  private static final String CURRENCY_CODE_EUR = "EUR";
+  private static final String CURRENCY_CODE_USD = "USD";
+
   private ConversionService service;
   @Mock
   private ExchangeRatesApiClient client;
@@ -54,47 +61,54 @@ class ConversionServiceImplTest {
 
   @Test
   void returnTransactionData_when_ConvertBRLToUSD(@Random UUID userId,
-                                                  @Random UUID transactionId,
-                                                  @Random BigDecimal convertedAmount,
-                                                  @Random BigDecimal conversionRate) {
+                                                  @Random UUID transactionId) {
 
     ConversionRequest request = ConversionRequest.builder()
                                                  .withUserId(userId)
-                                                 .withDesiredCurrency("BRL")
-                                                 .withOrigin(MonetaryAmount.builder()
-                                                                           .withAmount(TEN)
-                                                                           .withCurrency("BRL")
-                                                                           .build())
+                                                 .withDesiredCurrency(CURRENCY_CODE_BR)
+                                                 .withOrigin(buildMonetaryAmount(TEN,
+                                                                                 CURRENCY_CODE_USD))
                                                  .build();
 
-    LocalDateTime date = ofInstant(ofEpochMilli(1624076343L), ZoneId.of("UTC"));
-    Map<String, BigDecimal> rates = new HashMap<>();
-    rates.put("BRL", new BigDecimal(2));
-    when(client.latest(any(), any())).thenReturn(RatesResponse.builder()
-                                                              .withSuccess(true)
-                                                              .withTimestamp(1624076343L)
-                                                              .withDate(date.toLocalDate())
-                                                              .withRates(rates)
-                                                              .build());
+    LocalDateTime date = ofInstant(ofEpochMilli(1624076343L),
+                                   ZoneId.of(TIME_ZONE_UTC));
+    mockCallClientLatest(date);
 
     ConversionResponse response = service.convert(request);
 
     assertThat(response).isNotNull();
     assertThat(response.getUserId()).isEqualTo(userId);
-    assertThat(response.getTransactionId()).isEqualTo(userId);
+//    assertThat(response.getTransactionId()).isEqualTo(transactionId);
     assertThat(response.getTransactionDate()).isEqualTo(date);
-    assertThat(response.getConversionRate()).isEqualTo(rates.get("BRL"));
+    assertThat(response.getUsedConversionRate()).isEqualTo(RATE_USD_TO_EUR.divide(RATE_BRL_TO_EUR));
     assertThat(response.getConverted()).isNotNull();
-    assertThat(response.getConverted()
-                       .getCurrency()).isEqualTo("BRL");
-    assertThat(response.getConverted()
-                       .getAmount()).isEqualTo(conversionRate.multiply(MonetaryAmount.builder()
-                                                                                     .withAmount(TEN)
-                                                                                     .withCurrency("BRL")
-                                                                                     .build()
-                                                                                     .getAmount()));
+    assertThat(response.getConverted()).isEqualTo(buildMonetaryAmount(new BigDecimal(4.0).setScale(1),
+                                                                      CURRENCY_CODE_BR));
 
-    verify(client, times(1)).latest(any(), any());
+    verify(client, times(1)).latest(any(), any(), any());
+  }
+  
+
+  private MonetaryAmount buildMonetaryAmount(BigDecimal amount, String currency) {
+    return MonetaryAmount.builder()
+                         .withAmount(amount)
+                         .withCurrency(currency)
+                         .build();
+  }
+
+  private void mockCallClientLatest(LocalDateTime date) {
+
+    when(client.latest(any(), any(), any())).thenReturn(RatesResponse.builder()
+                                                                     .withSuccess(true)
+                                                                     .withTimestamp(1624076343L)
+                                                                     .withDate(date.toLocalDate())
+                                                                     .withRates(Map.of(CURRENCY_CODE_BR,
+                                                                                       RATE_BRL_TO_EUR,
+                                                                                       CURRENCY_CODE_EUR,
+                                                                                       ONE,
+                                                                                       CURRENCY_CODE_USD,
+                                                                                       RATE_USD_TO_EUR))
+                                                                     .build());
   }
 
 }
