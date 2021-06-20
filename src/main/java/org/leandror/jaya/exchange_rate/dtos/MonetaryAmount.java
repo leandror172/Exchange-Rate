c370eddb71ebdfe78fac71b2622c902295063a5f
Leandro Rocha <leandrocdarocha@gmail.com>
2021-06-20 13:02:39 -0300

    Clean up

--- src/main/java/org/leandror/jaya/exchange_rate/dtos/MonetaryAmount.java
+++ src/main/java/org/leandror/jaya/exchange_rate/dtos/MonetaryAmount.java
@@ -19,7 +19,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @Data
